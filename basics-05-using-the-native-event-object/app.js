5ad131d6ff889dd8a7f32fff4b20c57514c86f90
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-11-22 15:23:52 -0300

    Add files via upload

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,59 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      lastName: "",
+      // fullname: "",
+    };
+  },
+  watch: {
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + "" + value;
+    //   }
+    //},
+  },
+  computed: {
+    fullname() {
+      console.log("Running again...");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
+  methods: {
+    outputFullName() {
+      console.log("Running again...");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Mendes";
+    },
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");

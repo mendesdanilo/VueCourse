da106440082233c77ce7fe91fc0e7251add2cf2f
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-11-30 15:50:27 -0300

    Add files via upload

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "55555",
+          email: "manuel@email.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Silva",
+          phone: "7777777",
+          email: "julie@email.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+</style>

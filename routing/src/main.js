bbb0380c01a594eedbfa15efc7dcb9af749745fc
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-12-10 13:45:25 -0300

    Add files via upload

--- routing/src/main.js
+++ routing/src/main.js
@@ -9,9 +9,10 @@ import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList }, //our-domain.com/teams => loads a component
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers },
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
   ],
   linkActiveClass: 'active',
 });

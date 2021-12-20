27782999a434326134c6a2109b5386fd657fa642
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-12-20 16:10:22 -0300

    Add files via upload

--- CoachesVue/src/pages/requests/RequestsReceived.vue
+++ CoachesVue/src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,36 @@
 <template>
-  <section>request</section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul></ul>
+      <h3>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    receivedRequests() {},
+  },
+};
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>

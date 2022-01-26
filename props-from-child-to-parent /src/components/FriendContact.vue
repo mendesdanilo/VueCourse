91c9b2fab3d1c0b3072e101a8820c3007b26f219
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2022-01-26 10:07:22 -0300

    Add files via upload

---
+++ props-from-child-to-parent /src/components/FriendContact.vue
@@ -0,0 +1,44 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : " " }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      //to pass props from child to parent, create a new data property and set it to equal to the received prop value
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
+      }
+    },
+  },
+};
+</script>

e3889e02fda5da3188d89879f6908e5d861f5c42
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2022-01-27 18:03:28 -0300

    Add files via upload

---
+++ delete-friend/src/components/FriendContact.vue
@@ -0,0 +1,75 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : " " }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ["toggle-favorite", "delete"],
+  // emits: {
+  //   "toggle-favorite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing!");
+  //       return false;
+  //     }
+  //   },
+  // },
+  data() {
+    return {
+      detailsAreVisible: false,
+      //to pass props from child to parent, create a new data property and set it to equal to the received prop value
+      // friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>

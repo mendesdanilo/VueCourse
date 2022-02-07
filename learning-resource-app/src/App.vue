ea2c638ad011ea2299ca3a97d3b73dbd12bb4221
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2022-02-07 16:05:28 -0300

    Add files via upload

---
+++ learning-resource-app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'Yada Yada',
+          link: 'www.vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'www.google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
